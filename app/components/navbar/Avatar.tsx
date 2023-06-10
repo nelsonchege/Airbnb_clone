12a7c8d5ad2bdbb7d9fd10b7ab06ff6d6b4f8f0b
nelson chege <nelson3chege@gmail.com>
2023-06-10 03:27:57 +0300

    update: create the navbar

---
+++ app/components/navbar/Avatar.tsx
@@ -0,0 +1,14 @@
+"use client";
+import Image from "next/image";
+
+export default function Avatar() {
+  return (
+    <Image
+      alt="Avatar"
+      className="rounded-full"
+      height="30"
+      width="30"
+      src="/images/placeholder.jpg"
+    />
+  );
+}
