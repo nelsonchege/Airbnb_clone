eee1bfcc5b88dc0cedd2fc5b53175b96644dbb04
nelson chege <nelson3chege@gmail.com>
2023-06-13 02:49:58 +0300

    finish creating a modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import ClientOnly from "./clientOnly";
+import Modal from "./components/modals/Modal";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 import { Inter, Nunito } from "next/font/google";
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <Modal actionLabel="submit" isOpen title="Login Modal" />
           <Navbar />
         </ClientOnly>
         {children}
