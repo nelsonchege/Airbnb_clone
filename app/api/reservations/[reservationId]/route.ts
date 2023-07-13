bfbb38ced3132ae41c60c8d1ddaeb092a18564eb
nelson chege <nelson3chege@gmail.com>
2023-07-14 02:57:54 +0300

    adding api for deleting reservation

---
+++ app/api/reservations/[reservationId]/route.ts
@@ -0,0 +1,33 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import { NextResponse } from "next/server";
+import prisma from "@/app/lib/db";
+
+type IParams = {
+  reservationId?: string;
+};
+
+export async function DELETE(
+  requset: Request,
+  { params }: { params: IParams }
+) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { reservationId } = params;
+
+  if (!reservationId || typeof reservationId !== "string") {
+    throw new Error("Invalid ID");
+  }
+
+  const Reservation = await prisma.reservation.deleteMany({
+    where: {
+      id: reservationId,
+      OR: [{ userId: currentUser.id }, { listing: { userId: currentUser.id } }],
+    },
+  });
+
+  return NextResponse.json(Reservation);
+}
